59adc7c95382cecff1375d0d7cd356813a596f09
Asad Hayat <asadhayat.1068@gmail.com>
2024-03-07 04:20:45 +0500

    Switched to chi Router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,15 +5,20 @@ import (
 
 	"github.com/asadhayat1068/gowebdev_toptal_udemy/pkg/config"
 	"github.com/asadhayat1068/gowebdev_toptal_udemy/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v4"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// // Using pat
+	// mux := pat.New()
+	// // Routes
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	// Routes
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// Using chi
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/asadhayat1068/gowebdev_toptal_udemy
 
 go 1.21.4
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v4 v4.1.3
